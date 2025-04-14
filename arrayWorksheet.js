bd98b74269e7fbddd0ae879b3288bf1de640b027
DC <curtis.deshawn@ymail.com>
2025-04-14 01:28:30 +0000

    part 2 complete

--- arrayWorksheet.js
+++ arrayWorksheet.js
@@ -1,4 +1,35 @@
 
 
 
-//P
+//Part 1 
+
+let fruits = ["apple", "banana", "cherry"];
+
+console.log(fruits[1]); //Value of array at index 1
+
+console.log(fruits.length); /// length of the array 
+
+//Part 2
+
+fruits.push("orange"); /// adds "orange to array"
+
+console.log(fruits); 
+
+fruits.pop(); // removes last element from array
+
+console.log(fruits);
+
+//challenge
+
+fruits.push("avocado"); // adds "avocado"
+
+fruits.push ("coconut"); // adds "coconut"
+
+console.log(fruits);
+
+fruits.pop();
+
+console.log(fruits);
+
+console.log(fruits.length);
+
